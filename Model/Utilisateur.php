2016cf604889b157d1cf3d35de1e6054b2523834
ErdalKARAER <e.karaer@lprs.fr>
2021-02-20 00:33:42 +0100

    Création fonction hydrate_setter_et_getter

---
+++ Model/Utilisateur.php
@@ -0,0 +1,83 @@
+<?php
+
+class utilisateur{
+
+  private $prenom;
+  private $nom;
+  private $numero_mail;
+  private $confirmation_numero_mail;
+  private $pwd;
+
+
+  public function __construct($donnees){
+    $this->hydrate($donnees);
+  }
+
+  public function hydrate(array $donnees)
+  {
+    foreach ($donnees as $key => $value)
+    {
+      $method = 'set'.ucfirst($key);
+      if (method_exists($this, $method))
+      {
+        $this->$method($value);
+      }
+    }
+  }
+
+  public function getNom()
+  {
+    return $this->nom;
+  }
+  public function getPrenom()
+  {
+    return $this->prenom;
+  }
+  public function getNumero_mail()
+  {
+    return $this->$numero_mail;
+  }
+  public function getConfirmation_numero_mail()
+  {
+    return $this->$confirmation_numero_mail;
+  }
+  public function getPwd()
+  {
+    return $this->pwd;
+  }
+
+
+  public function setNom($nom){
+    if (is_string($nom))
+    {
+      $this->nom = $nom;
+    }
+  }
+    public function setPrenom($prenom){
+      if (is_string($prenom))
+      {
+        $this->prenom = $prenom;
+      }
+    }
+
+      public function setNumero_mail($numero_mail){
+        if (is_string($numero_mail))
+        {
+          $this->numero_mail = $numero_mail;
+        }
+      }
+      public function setConfirmation_numero_mail($confirmation_numero_mail){
+        if (is_string($confirmation_numero_mail))
+        {
+          $this->confirmation_numero_mail = $confirmation_numero_mail;
+        }
+      }
+      public function setPwd($pwd){
+        if (is_string($pwd))
+        {
+          $this->pwd = $pwd;
+        }
+      }
+
+}
+?>
